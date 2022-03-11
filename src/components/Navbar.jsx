459f6dcba09f375fbc5463af270adc692f6ccc05
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2022-03-11 19:41:38 +0700

    Minor padding changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { FaClipboardCheck } from "react-icons/fa";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-light bg-light ps-4">
+    <nav className="navbar navbar-light bg-light ps-4 py-3">
         <div className="container-fluid">
           <span className="navbar-brand mb-0 h1 ms-3">
             <FaClipboardCheck className="nav-logo"/>
